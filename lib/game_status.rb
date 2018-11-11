509477834f18f3279b45543298f8d00a332af7cf
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-11 19:39:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,12 +36,16 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board).any? { |pos| board[pos] == "X" }
-    "X"
-  elsif won?(board).any? { |pos| board[pos] == "O" }
-    "O"
-  elsif !won?(board)
-    nil
+  token = " "
+  won?(board).each do |pos|
+    if board[pos] == "X"
+      token == "X"
+    elsif board[pos] == "O"
+      token == "O"
+    else
+      token == nil
+    end
   end
+  return token
 end
 
